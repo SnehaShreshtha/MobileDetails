bac9e4b7dae161e4da4dd221bc9ebc6a37434781
SnehaShreshtha <snehashreshtha.rd@gmail.com>
2016-04-19 00:02:40 +0530

    resolve issue JSON

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	$.getJson("phones.json",function(mobdata){
+	
+	$.getJSON("phones.json",function(mobdata){
 		
 		$.each(mobdata,function(key,value){
-          $('ul').append("<li>"+value.name+"</li>");
+          $('ul').append("<li id='"+key+ "'>" +value.name+ "</li>");
 		});
 	
 	});
