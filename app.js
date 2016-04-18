1b8a9a1ac9b4af827b15846239fa6876dc88db95
SnehaShreshtha <snehashreshtha.rd@gmail.com>
2016-04-19 01:50:33 +0530

    fetching data from json - initial

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 	$.getJSON("phones.json",function(mobdata){
 		
 		$.each(mobdata,function(key,value){
-          $('ul').append("<li id='"+key+ "'>" +value.name+ "</li>");
+          $('ul').append("<p id='mob-name'>" +value.name+ "</p>");
+          $('ul').append("<p id='mob-des'>" +value.snippet+ "</p>");
+          $('ul').append("<img id='mob-image' src='"+value.imageUrl+"' alt='"+key+"'>");
 		});
 	
 	});
