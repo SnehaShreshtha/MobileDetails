7908e349c938c07931f246b8b36ddf7f20a4327d
SnehaShreshtha <snehashreshtha.rd@gmail.com>
2016-04-19 18:30:35 +0530

    Text and Image formating

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
 	$.getJSON("phones.json",function(mobdata){
 		
 		$.each(mobdata,function(key,value){
-          $('ul').append("<p id='mob-name'>" +value.name+ "</p>");
-          $('ul').append("<p id='mob-des'>" +value.snippet+ "</p>");
-          $('ul').append("<img id='mob-image' src='"+value.imageUrl+"' alt='"+key+"'>");
+          $('ul').append("<li class='details'>" 
+          	              + "<a href='#' class='thumb-img'>"
+          	              + "<img class='mob-image' src='"+value.imageUrl+"' alt='"+key+"'>"
+          	              + "</a>"
+          	              + "<a href='#' class='mob-name'>"
+          	              + value.name
+          	              + "</a>"
+          	              + "<p class='mob-dec'>" +value.snippet+ "</p>"
+                          + "</li>");
 		});
 	
 	});
